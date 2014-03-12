819d7908abc0296dee78955ed3ed2cc1557c7439
darknautic <blue.darknautic@gmail.com>
2014-03-12 08:11:59 -0600

    adding sign out feature

--- appZ.js
+++ appZ.js
@@ -1,6 +1,5 @@
 if (Meteor.isClient) {
 
-  //Meteor.subscribe("userNames");
 
   Template.login.events({
       
@@ -11,43 +10,47 @@ if (Meteor.isClient) {
           var email = t.find('#login-email').value
           var password = t.find('#login-password').value;
 
-        // Trim and validate your fields here.... 
+          // Trim and validate your fields here.... 
 
-        // If validation passes, supply the appropriate fields to the
-        // Meteor.loginWithPassword() function.
-        Meteor.loginWithPassword(email, password, function(err){
-        if (err) {}
-          // The user might not have been found, or their passwword
-          // could be incorrect. Inform the user that their
-          // login attempt has failed. 
-        else {}
-          // The user has been logged in.
-          //console.log(Meteor.users.find({_id: this.userId}));
-          console.log("the user is :" + Meteor.userId());
-          console.log("the user mail :" + Meteor.user().emails[0].address);
+          // If validation passes, supply the appropriate fields to the
+          // Meteor.loginWithPassword() function.
+          Meteor.loginWithPassword(email, password, function(err){
+              if (err) {
+                console.log("Not login. Error !¡"); 
+              }
+                // The user might not have been found, or their passwword
+                // could be incorrect. Inform the user that their
+                // login attempt has failed. 
+              else {
+
+                  // The user has been logged in.
+                  //console.log("the user is :" + Meteor.userId());
+                  console.log("the user mail :" + Meteor.user().emails[0].address);
+                  
+                  // Hash Value
+                  /*var current_date = (new Date()).valueOf().toString();
+                  var random = Math.random().toString();
+                  console.log(crypto.createHash('sha1').update(current_date + random).digest('hex'));
+                  */
+                                    
+                  //Session.set('SessionID',Meteor.userId());
+                  Session.set('loggedUser',Meteor.user().emails[0].address);                  
+                  console.log("Successfully login");                  
+                  alert("run !!");
+
+              }              
+                
+            
+
+          });
+
           
-          var hashToken = Meteor.user();
-            //{'emails.address' : "s@s"},{'services.resume.loginTokens.hashedToken' : 1 })[0];
-          hashToken = hashToken;
-
-          console.log(hashToken);
-          //console.log("the user mail :" + Meteor.user({services : { resume : { loginTokens : {}}}}));
-          //console.log("the user mail :" + Meteor.user(     {'emails.address':"w@w"},{_id:1}    )._id);
-
-          var time = new Date();
-          Session.set('sessionID',Meteor.userId());
-          console.log("login succes");
-          console.log("should set a session ?");
-          alert("run !!");
-          //console.log(Meteor.users.find({ emails: { $elemMatch: { address: email } } }));
-
-        });
-
-          console.log("hi !!" + email); 
           return false; 
       }
 
-  });
+  });  
+
+
 
 Template.register.events({
     'submit #register-form' : function(e, t) {
@@ -80,7 +83,7 @@ Template.register.events({
     }
 
 
-
+/*
   Meteor.autorun(function() {
     // Whenever this session variable changes, run this function.
     var message = Session.get('displayMessage');
@@ -93,30 +96,47 @@ Template.register.events({
       Session.set('displayMessage', null);
     }
   });
-
+*/
 
 
 Template.signControls.events({
-  'click input' : function(){
-    console.log('singOut');
-    
-      Meteor.logout(function(err){
+  'click input' : function(){ 
+    Meteor.logout(function(err){
         if (err) {
+          console.log("Err - signOut Process");
           console.log(err);
-          alert('signout err');
+          //alert('Err - signOut Process');
         }
         else{
-          console.log("logout succes");
-          Session.set('sessionID',null);
-          delete Session.keys['sessionID'];
-          alert('signout success');
+          //Session.set('loggedUser',null);
+          delete Session.keys['loggedUser'];          
+          //console.log("Ok - signoOut successfully");
+          //alert('Ok - signoOut successfully');
         }
 
-      });
+    });
 
   }
 });
 
+Template.whois.events({
+  'click a#whois' : function(){
+    Meteor.logout(function(err){
+        if (err) {
+          console.log("Err - signOut Process");
+          console.log(err);
+          //alert('Err - signOut Process');
+        }
+        else{
+          //Session.set('loggedUser',null);
+          delete Session.keys['loggedUser'];          
+          //console.log("Ok - signoOut successfully");
+          //alert('Ok - signoOut successfully');
+        }
+
+    });
+  }
+});
 
 
 
@@ -124,21 +144,28 @@ Meteor.startup(function() {
     Session.set("contentRenderedCount", 0);
   });
 
-  Template.content.rendered = function() {
+Template.content.rendered = function() {
     var renderCount = Session.get("contentRenderedCount") + 1;
     Session.set("contentRenderedCount", renderCount);
     document.getElementById("renderCount").innerText = renderCount;
   };
 
-  Template.userUpdate.events = {
+Template.userUpdate.lastUpdated = function() {
+    return Meteor.user();
+  };
+
+Template.userUpdate.events = {
     "click #updateUser": function() {
       Meteor.users.update({_id: Meteor.userId()}, {$set: {lastActive: new Date()}});
     }
   };
 
-  Template.userUpdate.lastUpdated = function() {
-    return Meteor.user();
+Template.whois.loggedUser = function(){
+  var whois = Session.get('loggedUser');
+  //if(!whois){whois = "Sign out";}
+  return whois;
   };
+
 
  /* Meteor.autorun(function() {
     Session.set("meteor_loggedin",!!Meteor.user());
@@ -149,6 +176,7 @@ Meteor.startup(function() {
   });
 */
 
+
 }  //isClient
 
 
@@ -157,17 +185,12 @@ if (Meteor.isServer) {
 
   Meteor.startup(function () {
     // code to run on server at startup
-
-
-  });
+    });
 
   Meteor.users.allow({
     'update': function () {
       return true; 
-    }
-  });
+      }
+    });
 
-
-
-
-} //isServer
+  } //isServer
